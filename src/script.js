b88a200a1802c32fd48edaa336af927eb052f921
kkrawczynski <kkrawczynski@it-dev.pl>
2018-08-13 16:29:52 +0200

    custom REST API script

--- src/script.js
+++ src/script.js
@@ -1,81 +1,103 @@
+//var rootUrl = "https://kksandbox.sharepoint.com/sites/intra_uk";
+
 // Example from:
 // https://docs.microsoft.com/en-us/sharepoint/dev/general-development/how-to-follow-documents-sites-and-tags-by-using-the-rest-service-in-sharepoint-2#code-example-get-followed-content-by-using-the-sharepoint-rest-service
 
-var followingManagerEndpoint;
-var followedCount;
+// failed - no napa anymore, error in the script
 
-// Get the SPAppWebUrl parameter from the query string and build
-// the following manager endpoint.
-$(document).ready(function () {
+// *** reusable content ***/
+// container test
+// var testContainer = document.getElementById("test");
 
-    // container test
-    var testContainer = document.getElementById("test");
-    testContainer.innerHTML = "Some content from the script.js file";
+// $(document).ready(function () {})
+// $("#test").html(followedList);
 
-    // var appweburl;
-    var appweburl = "https://kksandbox.sharepoint.com/sites/intra_uk/";
-    var params = document.URL.split("?")[1].split("&amp;");
-    for (var i = 0; i < params.length; i = i + 1) {
-        var param = params[i].split("=");
-        if (param[0] === "SPAppWebUrl") appweburl = param[1];
-    }
-    followingManagerEndpoint = decodeURIComponent(appweburl) + "/_api/social.following";
-    getMyFollowedCount();
+
+
+
+
+
+
+// var followingManagerEndpoint;
+// var followedCount;
+
+// // Get the SPAppWebUrl parameter from the query string and build
+// // the following manager endpoint.
+// $(document).ready(function () {
+
+//     // container test
+//     //var testContainer = document.getElementById("test");
+//     //testContainer.innerHTML = "Some content from the script.js file";
+
+//     // var appweburl;
+//     
+
+
+//     var params = document.URL.split("?")[1].split("&amp;");
+//     for (var i = 0; i < params.length; i = i + 1) {
+//         var param = params[i].split("=");
+//         if (param[0] === "SPAppWebUrl") appweburl = param[1];
+//     }
+//     followingManagerEndpoint = decodeURIComponent(appweburl) + "/_api/social.following";
+//     getMyFollowedCount();
     
-});
+// });
 
-// Get the count of content that the current user is following.
-// The "types=14" parameter specifies all content types
-// (documents = 2 + sites = 4 + tags = 8).
-function getMyFollowedCount() {
-    $.ajax({
-        url: followingManagerEndpoint + "/my/followedcount(types=14)",
-        headers: {
-            "accept": "application/json;odata=verbose"
-        },
-        success: function (data) {
-            followedCount = data.d.FollowedCount;
-            getMyFollowedContent();
-        },
-        error: requestFailed
-    });
-}
+// // Get the count of content that the current user is following.
+// // The "types=14" parameter specifies all content types
+// // (documents = 2 + sites = 4 + tags = 8).
+// function getMyFollowedCount() {
+//     $.ajax({
+//         url: followingManagerEndpoint + "/my/followedcount(types=14)",
+//         headers: {
+//             "accept": "application/json;odata=verbose"
+//         },
+//         success: function (data) {
+//             followedCount = data.d.FollowedCount;
+//             getMyFollowedContent();
+//         },
+//         error: requestFailed
+//     });
+// }
 
-// Get the content that the current user is following.
-// The "types=14" parameter specifies all content types
-// (documents = 2 + sites = 4 + tags = 8).
-function getMyFollowedContent() {
-    $.ajax({
-        url: followingManagerEndpoint + "/my/followed(types=14)",
-        headers: {
-            "accept": "application/json;odata=verbose"
-        },
-        success: followedContentRetrieved,
-        error: requestFailed
-    });
-}
+// // Get the content that the current user is following.
+// // The "types=14" parameter specifies all content types
+// // (documents = 2 + sites = 4 + tags = 8).
+// function getMyFollowedContent() {
+//     $.ajax({
+//         url: followingManagerEndpoint + "/my/followed(types=14)",
+//         headers: {
+//             "accept": "application/json;odata=verbose"
+//         },
+//         success: followedContentRetrieved,
+//         error: requestFailed
+//     });
+// }
 
-// Parse the JSON data and iterate through the collection.
-function followedContentRetrieved(data) {
-    var stringData = JSON.stringify(data);
-    var jsonObject = JSON.parse(stringData);
-    var types = {
-        1: "document",
-        2: "site",
-        3: "tag"
-    };
+// // Parse the JSON data and iterate through the collection.
+// function followedContentRetrieved(data) {
+//     var stringData = JSON.stringify(data);
+//     var jsonObject = JSON.parse(stringData);
+//     var types = {
+//         1: "document",
+//         2: "site",
+//         3: "tag"
+//     };
 
-    var followedActors = jsonObject.d.Followed.results;
-    var followedList = "You're following " + followedCount + " items:";
-    for (var i = 0; i < followedActors.length; i++) {
-        var actor = followedActors[i];
-        // string contained errors
-        followedList += "<p>The " + types[actor.ActorType] + ": \\" +
-            actor.Name + "\\</p>";
-    }
-    $("#message").html(followedList);
-}
+//     var followedActors = jsonObject.d.Followed.results;
 
-function requestFailed(xhr, ajaxOptions, thrownError) {
-    alert('Error:\\n' + xhr.status + '\\n' + thrownError + '\\n' + xhr.responseText);
-}
+//     var followedList = "some text"
+//     // buggy code from the example
+//     // var followedList = "You're following " + followedCount + " items:";
+//     // for (var i = 0; i < followedActors.length; i++) {
+//     //     var actor = followedActors[i];
+//     //     followedList += "<p>The " + types[actor.ActorType] + ": \\"" +
+//     //     actor.Name + "\\"</p>";
+//     // } 
+    
+//     $("#test").html(followedList);
+// }
+
+// function requestFailed(xhr, ajaxOptions, thrownError) {
+//     alert('Error:\\n' + xhr.status + '\\n' + thrownError + '\\n' + xhr.responseText);
+// }
